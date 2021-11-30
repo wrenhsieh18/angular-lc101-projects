1da97a88e521e91e86f14a52127aa4e19abb3af7
Wren Hsieh <wrenhsieh18@gmail.com>
2021-11-29 20:41:28 -0600

    exP1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My favorite animals';
+  image1 = 'https://www.gannett-cdn.com/-mm-/9e1f6e2ee20f44aa1f3be4f71e9f3e52b6ae2c7e/c=0-110-2121-1303/local/-/media/2020/06/22/USATODAY/usatsports/MotleyFool-TMOT-3c937e85-a-tortoise.jpg?width=2121&height=1193&fit=crop&format=pjpg&auto=webp';
+  image2 = 'https://www.aces.edu/wp-content/uploads/2018/11/iStock-182344013.jpg';
+  image3 = 'https://files.worldwildlife.org/wwfcmsprod/images/African_Elephant_Kenya_112367/story_full_width/qxyqxqjtu_WW187785.jpg';
 
   constructor() { }
 
