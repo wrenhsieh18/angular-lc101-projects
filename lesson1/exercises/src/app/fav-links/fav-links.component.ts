779750f2a650d74500f89875d40f1fac2a16694a
Wren Hsieh <wrenhsieh18@gmail.com>
2021-11-29 21:04:04 -0600

    finishedEXP2

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks: string[] = ["https://www.launchcode.org", "https://www.sugarwitchic.com"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
