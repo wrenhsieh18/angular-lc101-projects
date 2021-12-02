3927dcc47eb7262565efe15260f615509a93c198
Wren Hsieh <wrenhsieh18@gmail.com>
2021-12-02 09:43:37 -0600

    finished lesson2EX

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,6 +25,9 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
+  addToCrew (selectedCandidate) {
+    this.crew.push(selectedCandidate);
+  }
 
   // BONUS: Code the changeMissionName function here:
 
